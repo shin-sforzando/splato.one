dba2c14fecf458c4bbb81eeaa995ab2329bc93f8
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-08-17 00:57:07 +0900

    feat: :+1: add storybook-dark-mode

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     'storybook-addon-next',
+    'storybook-dark-mode',
   ],
   staticDirs: ['../public'],
   framework: '@storybook/react',
